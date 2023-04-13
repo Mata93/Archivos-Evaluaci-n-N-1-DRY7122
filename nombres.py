c00d6655e4c9bde55a0beb8988487df9b35b27cd
Mata93 <danil.lopezm@duocuc.cl>
2023-04-13 13:59:31 +0000

    Se modifico el codigo nombres.py

--- nombres.py
+++ nombres.py
@@ -1,4 +1,4 @@
 n1 = input("Ingresa el nombre del primer integrante del grupo: ")
 n2 = input("Ingresa el nombre del segundo integrante del grupo: ")
-
-print("Evaluacion N°1 Programacion y Redes Virtualizadas ", n1, n2)
+n3 = input("Ingresa el nombre del tercer integrante del grupo: ")
+print("Evaluacion N°1 Programacion y Redes Virtualizadas ", n1, n2, n3)
